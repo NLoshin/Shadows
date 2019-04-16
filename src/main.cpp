132c8e1f11566aa089bf487df1551dc4f8c2334e
Dima Barsukov <oldreddemon@gmail.com>
2019-04-16 17:36:52 +0300

    [ADD] PWM for servo motors

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,24 @@
 #define SCENECOUNT 9
 
 
+#define SERVOCOUNT 3
+#define Y 1
+
+#define angleCoef 23 // 4096 / 180
 
 // Servo motors initialization with Adafruit PWM library
 Adafruit_PWMServoDriver servo = Adafruit_PWMServoDriver();
 
 // Structure for position variables
-struct position {uint8_t x; uint8_t y;};
+struct position {byte x; byte y;};
+
+// Structure for servos
+struct servoStruct {
+  byte pwmPin;
+  int SAFEZONES[3][2];
+  // Automatic variables 
+  Adafruit_PWMServoDriver pwm;
+};
 
 // Structure for scenes
 struct sceneStruct {
@@ -26,17 +38,16 @@ struct sceneStruct {
   // Automatic variables
   unsigned long time;
   float coefX;
-  float coefY;
+  float coefZ;
   float coefNoise;
   float coefPixels;
-  float coefTime;
 };
 
 // Class for show scenes
 class sceneClass {
   private:
   // PRIVATE VARIABLES=======
-  uint8_t id = 0;
+  byte id = 0;
 
   const long shadowsText[10] = {
     0b111101001001100110001111010001,
@@ -53,14 +64,8 @@ class sceneClass {
 
   // Function for display the storm
   void storm(sceneStruct scene) { 
-
-
-      matrix.drawPixel(w,  0, matrix.Color(255, 255, 255));
-      matrix.drawPixel(w,  9, matrix.Color(255, 255, 255));
     // TODO: Storm function
   }
-   matrix.fillCircle(0,  9, 3, matrix.Color(255, 255, 255));
-   if (id != SCENECOUNT-1) matrix.fillCircle(29, 9, 3, matrix.Color(255, 255, 255));
   
   // Function for circuit frame
   void circuit(sceneStruct scene) {
@@ -71,34 +76,36 @@ class sceneClass {
     }
   }
 
+  // Function for move servos
+  void updateServos(sceneStruct scene) {
     }
-  }
-
-  // Function for check distance from UltraSonic
-  bool checkDist() {
-    digitalWrite(TRIGPIN,1);
-    delayMicroseconds(10);
-    digitalWrite(TRIGPIN,0);
-    
-    int distance = pulseIn(ECHOPIN, 1, 3000) / 58;
-    return distance > 0;
   }
 
   // MAIN CODE===============
   public:
+  sceneStruct scenes[SCENECOUNT];
+  servoStruct servos[SERVOCOUNT];
   // Initializate scene structures
-  void init(sceneStruct sceneConfig[SCENECOUNT]) {
-    for(int i; i<SCENECOUNT; i++) {
-      scenes[i] = sceneConfig[i];
+  void init(sceneStruct sceneStructs[SCENECOUNT], servoStruct servoStructs[SERVOCOUNT]) {
+    for(int i=0; i<SCENECOUNT; i++) {
+      scenes[i] = sceneStructs[i];
       if (scenes[i].startTime < 1000) scenes[i].startTime *= 1000;
       if (scenes[i].endTime   < 1000) scenes[i].endTime   *= 1000;
-      int time = millis(); 
+      unsigned int time = millis(); 
       scenes[i].startTime = scenes[i].startTime + time;
       scenes[i].endTime   = scenes[i].endTime   + time;
       scenes[i].time = scenes[i].endTime - scenes[i].startTime;
       scenes[i].coefX = 1.0 * (scenes[i].end.x - scenes[i].start.x) / scenes[i].time;
       scenes[i].coefZ = 1.0 * (scenes[i].end.y - scenes[i].start.y) / scenes[i].time;
     }
+    for (int i=0; i < SERVOCOUNT; i++) {
+      servos[i] = servoStructs[i];
+      servos[i].pwm = Adafruit_PWMServoDriver(servos[i].pwmPin);
+      servos[i].pwm.begin();
+      servos[i].pwm.setPWMFreq(60);
+      for (int j=0; j < sizeof(servos[i].SAFEZONES); j++) {
+        for (int l=0; l < 2; l++) {
+          servos[i].SAFEZONES[j][l] = servos[i].SAFEZONES[j][l] * angleCoef; 
         }
       }
     }
@@ -111,10 +118,9 @@ class sceneClass {
     
     Serial.print("Update:\t");
     Serial.println(id);
-    // if (checkDist()) {
-    if (long(millis() - scenes[id].endTime) > 0) {
+
+    if (false) { // Book signal
       id++;
-      delay(3000);
       // player.next();
     }
   }
@@ -128,33 +134,33 @@ sceneClass scenes;
 void setup() {
   delay(5000);
   Serial.begin(9600);
-  ////// DISPLAY SETUP //////
-  matrix.begin();
-  // matrix.clear();
-  matrix.setBrightness(255);
   ////// CLASS SETUP //////
+  servoStruct servoStructs[SERVOCOUNT] {
+    {0x40},
+    {0x41},
     {0x42}};
 
-    {34, 49,   0b0000010, {{0, 0, 0}, {255, 0, 0}, {}}},
-    {52, 57,   0b0100010, {{0, 0, 0}, {255, 0, 0}, {}}, 3, {14, 4, 1}, {255, 50, 0}},
+  sceneStruct sceneStructs[SCENECOUNT] = {
+    {0,   20, 0b10},
+    {23,  31, 0b10},
 
-    {60, 62,   0b0100100, {},3},
-    {65, 69,   0b0100100, {}, 3},
-    {72, 75,   0b0100100, {}, 3, {14, 4, 1}, {255, 255, 0}},
+    {34,  49, 0b10},
+    {52,  57, 0b10},
 
-    {78, 101,  0b0000001, {{255, 0, 0}, {0, 0, 0}, {255, 0, 0}}},
-    {104, 120, 0b0100100, {}, 3}
+    {60,  62, 0b10},
+    {65,  69, 0b10},
+    {72,  75, 0b10},
+
+    {78,  101, 0b10},
+    {104, 120, 0b10}
   };                                                                                                                                                                                                               
-  scenes.init(sceneConfig);
-  // scenes.rainbowText();
-  // matrix.show();
+  scenes.init(sceneStructs, servoStructs);
+
   ///// SERVOS SETUP /////
   servo.begin();
+  servo.setPWMFreq(60);
+  delay(500);
 
-  ////// AUDIO MODEL SETUP //////
-  // Serial.begin(115200);
-  // player.volume(10);
-  // player.play();
   ///// ENGINES SETUP /////
   pinMode(ENGINE1, OUTPUT);
   pinMode(ENGINE2, OUTPUT);
